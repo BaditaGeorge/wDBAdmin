ca94b4cbbdc28eeeff3927b4c356de7e760f9b31
LivintLucian <48414867+LivintLucian@users.noreply.github.com>
2019-03-18 02:17:56 +0200

    work in progress

---
+++ Main/dropMenu.js
@@ -0,0 +1,12 @@
+let menuBtn = document.getElementById('generate');
+let menu = document.querySelector('.dropMenuGenerate');
+
+menuBtn.addEventListener('click', () => {
+    menu.classList.add('is--open');
+});
+
+document.addEventListener('keyup', (e) => {
+    if (e.key === 'Escape' || e.keyCode === 27) {
+        menu.classList.remove('is--open');
+    }
+})
