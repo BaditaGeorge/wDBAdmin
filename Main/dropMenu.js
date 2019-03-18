71717ce4d39dd0df8bf542781ed9215cd527a2d4
LivintLucian <48414867+LivintLucian@users.noreply.github.com>
2019-03-18 15:14:36 +0200

    leftSide mainPage

--- Main/dropMenu.js
+++ Main/dropMenu.js
@@ -1,12 +1,12 @@
-let menuBtn = document.getElementById('generate');
+let menuBtn = document.getElementById('generateButton');
 let menu = document.querySelector('.dropMenuGenerate');
 
 menuBtn.addEventListener('click', () => {
-    menu.classList.add('is--open');
+    menu.classList.add('isOpen');
 });
 
 document.addEventListener('keyup', (e) => {
     if (e.key === 'Escape' || e.keyCode === 27) {
-        menu.classList.remove('is--open');
+        menu.classList.remove('isOpen');
     }
 })
--- Main/main.js
+++ Main/main.js
@@ -10,3 +10,16 @@ document.addEventListener('keyup', (e) => {
         menu.classList.remove('is--open');
     }
 })
+
+let btnGenerate = document.getElementById('generateButton');
+let menuGenerate = document.querySelector('.dropMenuGenerate');
+
+btnGenerate.addEventListener('click', () => {
+    menuGenerate.classList.add('isOpen');
+});
+
+document.addEventListener('keyup', (e) => {
+    if (e.key === 'Escape' || e.keyCode === 27) {
+        menuGenerate.classList.remove('isOpen');
+    }
+})
