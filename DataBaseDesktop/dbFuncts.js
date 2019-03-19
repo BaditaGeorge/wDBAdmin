4df01d0adf5524685f3126c88e7e3350866ac9e2
BaditaGeorge <baditageorge@yahoo.com>
2019-03-20 00:29:43 +0200

    Some JS for DataBaseDesktop

---
+++ DataBaseDesktop/dbFuncts.js
@@ -0,0 +1,23 @@
+function Crt1(){
+    var a=document.createElement("a");
+    a.innerText="New Table";
+    document.getElementById("tabele").appendChild(a);
+}
+function Crt2()
+{
+    var a=document.createElement("a");
+    a.innerText="New Querie";
+    document.getElementById("queri").appendChild(a);
+}
+function Crt3()
+{
+    var a=document.createElement("a");
+    a.innerText="New Procedure";
+    document.getElementById("prceds").appendChild(a);
+}
+function Crt4()
+{
+    var a=document.createElement("a");
+    a.innerText="New Function";
+    document.getElementById("buton").appendChild(a);
+}
